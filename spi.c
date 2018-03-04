e59b0c9e7ba06d0c70d0054dd3b12319119ea491
Josh Harvey <josh@lamassu.is>
2018-03-04 19:24:33 +0000

    spi tweaks

--- spi.c
+++ spi.c
@@ -1,10 +1,9 @@
-#include <stdint.h>
 #include <stdio.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <sys/ioctl.h>
 #include <string.h>
-#include <linux/types.h>
 #include <linux/spi/spidev.h>
 
 #include "spi.h"
@@ -15,63 +14,64 @@ static uint8_t mode = 3;
 static uint8_t bits = 8;
 static uint32_t speed = 256000;
 static uint16_t delay;
+static int fd;
 
-static void pabort(const char *s) {
+static void pabort(const char *s, int exit_code) {
 	perror(s);
-	abort();
+	exit(exit_code);
 }
 
-void open(const char *spi_dev) {
+void OpenSpi(const char *spi_dev) {
+	int ret;
 	fd = open(spi_dev, O_RDWR);
-	if (fd<=0) {
-		printf("%s: Device %s not found\n", argv[0], argv[1]);
-		exit(1);
-	}
+
+	if (fd<=0)
+		pabort("can't open spi device", 1);
 
 	// spi mode
 	ret = ioctl(fd, SPI_IOC_WR_MODE, &mode);
 	if (ret == -1)
-		pabort("can't set spi mode");
+		pabort("can't set spi mode", 2);
 
 	ret = ioctl(fd, SPI_IOC_RD_MODE, &mode);
 	if (ret == -1)
-		pabort("can't get spi mode");
+		pabort("can't get spi mode", 2);
 
 	// bits per word
 	ret = ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
 	if (ret == -1)
-		pabort("can't set bits per word");
+		pabort("can't set bits per word", 2);
 
 	ret = ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &bits);
 	if (ret == -1)
-		pabort("can't get bits per word");
+		pabort("can't get bits per word", 2);
 
 	// max speed hz
 	ret = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
 	if (ret == -1)
-		pabort("can't set max speed hz");
+		pabort("can't set max speed hz", 2);
 
 	ret = ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed);
 	if (ret == -1)
-		pabort("can't get max speed hz");
+		pabort("can't get max speed hz", 2);
 }
 
-void light(uint8_t r, uint8_t g, uint8_t b, uint8_t a, unsigned char leds_buf[]) {
-	int i, fd, ret;
-
-  const leds_buf_size = kNumLeds * 4 + 8;
+void Light(uint8_t r, uint8_t g, uint8_t b, uint8_t a, unsigned char leds_buf[]) {
+  const int leds_buf_size = kNumLeds * 4 + 8;
 
   unsigned char wr_buf[leds_buf_size];
   unsigned char start_buf[4] = {0x0, 0x0, 0x0, 0x0};
   unsigned char end_buf[4] = {0xff, 0xff, 0xff, 0xff};
 
   memcpy(wr_buf, start_buf, 4);
-  int end_offset = 4 + num_leds * 4;
+  int end_offset = 4 + kNumLeds * 4;
   memcpy(wr_buf + end_offset, end_buf, 4);
 
 	if (write(fd, wr_buf, ARRAY_SIZE(wr_buf)) != ARRAY_SIZE(wr_buf)) {
 		perror("Write Error");
 	}
+}
 
-	return 0;
+int main (int argc, char **argv) {
+	OpenSpi("/dev/spi");
 }
--- spi.h
+++ spi.h
@@ -2,5 +2,5 @@
 
 const int kNumLeds = 26;
 
-void open(const char *spi_dev);
-void light(uint8_t r, uint8_t g, uint8_t b, uint8_t a, unsigned char leds_buf[]);
+void OpenSpi(const char *spi_dev);
+void Light(uint8_t r, uint8_t g, uint8_t b, uint8_t a, unsigned char leds_buf[]);
